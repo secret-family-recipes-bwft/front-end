d4aad1684717ad6224823c0c1c2cdf971d7f6cd5
Isaac Gorman <emailme@isaacg.net>
2020-07-30 18:12:27 -0500

    completed styling of success page. Up next => routing to recipe, and re-styling login and sign up page

--- sfra/src/components/RecipeForm/SuccessPage.js
+++ sfra/src/components/RecipeForm/SuccessPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import placeholderSuccess from "./placeholderSuccess.svg";
 
 const PageContainer = styled.div`
   width: 100vw;
@@ -29,7 +30,7 @@ const ProgressHeader = styled.p`
   font-family: "airbnb_cereal_appmedium";
   font-weight: normal;
   margin-left: 100px;
-  color: #0f9d58;
+  color: #317df6;
   // background: green;
 `;
 const ProgressBarDiv = styled.div`
@@ -49,153 +50,36 @@ const FormContainer = styled.div`
   height: 90vh;
   background-color: #fafafa;
   display: flex;
+  justify-content: center;
 `;
-
-const FieldsContainer = styled.div`
-  width: 60%;
-  height: 90vh;
-  background-color: white;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-`;
-// StatusLayoutDiv
-const StatusLayoutDiv = styled.div`
-  width: 80%;
+// SuccessDiv
+const SuccessDiv = styled.div`
+  width: 50%;
   height: 90vh;
   // background-color: pink;
   display: flex;
   flex-direction: column;
-  justify-content: flex-start;
   align-items: center;
 `;
 
-const StatusContainer = styled.div`
-  height: 350px;
-  width: 360px;
-  background: white;
+const SuccessIMG = styled.img`
+  width: 400px;
   margin-top: 40px;
 `;
 
-const Heading = styled.h1`
-  font-size: 20px;
+const SuccessHeading = styled.h1`
+  font-size: 24px;
   font-family: "airbnb_cereal_appmedium";
-  font-weight: normal;
-  text-align: left;
-  margin-bottom: 30px;
+  color: black;
 `;
 
-const StepBox = styled.div`
-  width: 100%;
-  height: 72px;
-  // background: red;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: flex-start;
-  margin-bottom: 24px;
-`;
-
-const StepNum = styled.h4`
-  font-size: 10px;
-  font-family: "airbnb_cereal_appmedium";
-  // font-weight: normal;
-  margin-bottom: 0px;
-  margin-top: 0px;
-`;
-
-const Title = styled.h4`
-  font-size: 16px;
-  font-family: "airbnb_cereal_appmedium";
-  font-style: normal;
-  font-weight: normal;
-  margin-bottom: 4px;
-  margin-top: 4px;
-`;
-
-const Overview = styled.h4`
-  font-size: 12px;
+const SuccessSubHeading = styled.p`
+  font-size: 18px;
   font-family: "airbnb_cereal_appbook";
-  font-weight: normal;
-  margin-bottom: 15px;
-  margin-top: 0px;
+  color: black;
+  margin-bottom: 40px;
 `;
-
-const StepDivider = styled.div`
-  width: 100%;
-  border: 1px solid #cbd2d9;
-`;
-
-// Next Step Signifier Styling
-const NextStepStepNum = styled.h4`
-  font-size: 10px;
-  font-family: "airbnb_cereal_appmedium";
-  // font-weight: normal;
-  margin-bottom: 0px;
-  margin-top: 0px;
-`;
-
-const NextStepTitle = styled.h4`
-  font-size: 16px;
-  font-family: "airbnb_cereal_appmedium";
-  font-style: normal;
-  font-weight: normal;
-  margin-bottom: 4px;
-  margin-top: 4px;
-  color: #4a87e8;
-`;
-
-const NextStepOverview = styled.h4`
-  font-size: 12px;
-  font-family: "airbnb_cereal_appbook";
-  font-weight: normal;
-  margin-bottom: 15px;
-  margin-top: 0px;
-  color: #4a87e8;
-`;
-
-const NextStepStepDivider = styled.div`
-  width: 100%;
-  border: 1px solid #4a87e8;
-`;
-// Next Step Signifier Styling
-
-const FormNavigation = styled.div`
-  width: 60%;
-  height: 10vh;
-  position: fixed;
-  bottom: 0;
-  background-color: white;
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  border-top: 1px solid #cbd2d9;
-`;
-
-const ButtonDiv = styled.div`
-  width: 600px;
-  height: 60px;
-  // background-color: pink;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const BackButton = styled.button`
-  width: 145px;
-  height: 40px;
-
-  background: white;
-  border-radius: 5px;
-  border: 1px solid #317df6;
-  color: #317df6;
-
-  font-size: 12px;
-  font-family: "airbnb_cereal_appmedium";
-  margin-right: 8px;
-`;
-
-const NextButton = styled.button`
+const SuccessButton = styled.button`
   width: 145px;
   height: 40px;
 
@@ -206,7 +90,13 @@ const NextButton = styled.button`
 
   font-size: 12px;
   font-family: "airbnb_cereal_appmedium";
-  margin-left: 8px;
+  font-weight: normal;
+  margin-bottom: 10px;
+`;
+const MaybeLater = styled.p`
+  font-size: 12px;
+  font-family: "airbnb_cereal_appmedium";
+  color: #317df6;
 `;
 
 export default function SuccessPage() {
@@ -219,7 +109,15 @@ export default function SuccessPage() {
       <ProgressBarDiv>
         <ProgressSignifier />
       </ProgressBarDiv>
-      <FormContainer></FormContainer>
+      <FormContainer>
+        <SuccessDiv>
+          <SuccessIMG src={placeholderSuccess} />
+          <SuccessHeading>All done!</SuccessHeading>
+          <SuccessSubHeading>You created your recipe!</SuccessSubHeading>
+          <SuccessButton>View Recipe</SuccessButton>
+          <MaybeLater>Maybe Later</MaybeLater>
+        </SuccessDiv>
+      </FormContainer>
     </PageContainer>
 
     // <div>
